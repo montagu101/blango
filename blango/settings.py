0653a5fdef301610d8beaf34df93cb22a3e58f07
distance <from_a_distance@hispeed.ch>
2024-04-27 12:30:27 +0200

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +132,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 #....this is set only for the course, remove when finished
 '''
--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from crispy_forms.layout import Submit
+from crispy_forms.helper import FormHelper
+
+
 from blog.models import Comment
 
 
@@ -6,3 +10,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
+
+    def __init__(self, *args, **kwargs):
+      super(CommentForm, self).__init__(*args, **kwargs)
+      self.helper = FormHelper()
+      self.helper.add_input(Submit('submit', 'Submit'))
