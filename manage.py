18c6841278cbce1f5b2a8577365ebb352b62d1f7
distance <from_a_distance@hispeed.ch>
2024-05-06 04:07:19 +0200

    auth added

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blango.settings')
-    os.environ.setdefault("DJANGO_CONFIGURATION", "Dev") #--added to read configurations
+    #os.environ.setdefault("DJANGO_CONFIGURATION", "Dev") #--added to read configurations
 
     try:
         #from django.core.management import execute_from_command_line
