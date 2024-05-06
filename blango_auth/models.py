638e915c5afd2a029ea5c929a2cdd501a416232a
distance <from_a_distance@hispeed.ch>
2024-05-06 04:37:39 +0200

    profile

--- blango_auth/models.py
+++ blango_auth/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 # Create your models here.
 
 
 class User(AbstractUser):
 pass
+
+
