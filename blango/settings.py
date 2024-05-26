ed02407dac1cf931704a878e85d19b3b2c8ffaad
distance <from_a_distance@hispeed.ch>
2024-05-26 13:45:40 +0200

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -11,191 +11,206 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os  
 from pathlib import Path  
+from datetime import timedelta
+
 from configurations import Configuration 
 from configurations import values  
 
-class Dev(Configuration):  
+class Dev(Configuration):
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.  
-  BASE_DIR = Path(__file__).resolve().parent.parent  
+    BASE_DIR = Path(__file__).resolve().parent.parent  
 
 
-  # Quick-start development settings - unsuitable for production  
-  # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/  
-  # SECURITY WARNING: keep the secret key used in production  secret! 
-  
-  SECRET_KEY = 'django-insecure-&!=9y436&^-bc$qia-mxngyf&xx)@ct)8lu@)=qxg_07-=z01w'  
-  # SECURITY WARNING: don't run with debug turned on in  production!  
-  DEBUG = True  
-  ALLOWED_HOSTS = ['*']  
+    # Quick-start development settings - unsuitable for production  
+    # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/  
+    # SECURITY WARNING: keep the secret key used in production  secret! 
+    
+    SECRET_KEY = 'django-insecure-&!=9y436&^-bc$qia-mxngyf&xx)@ct)8lu@)=qxg_07-=z01w'  
+    # SECURITY WARNING: don't run with debug turned on in  production!  
+
+    DEBUG = True 
+
+    ALLOWED_HOSTS = ['*', 'localhost']  
 
 
-  AUTH_USER_MODEL = "blango_auth.User"
-
-
-
- # X_FRAME_OPTIONS = 'ALLOW-FROM ' +  os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'  
- # CSRF_COOKIE_SAMESITE = None  
- # CSRF_TRUSTED_ORIGINS = [os.environ.get('CODIO_HOSTNAME') +  '-8000.codio.io']  
- # CSRF_COOKIE_SECURE = True  
- # SESSION_COOKIE_SECURE = True  
- # CSRF_COOKIE_SAMESITE = 'None'  
- # SESSION_COOKIE_SAMESITE = 'None'  
-
-
-  # Application definition  
-  INSTALLED_APPS = [ 
-      'django.contrib.admin',  
-      'django.contrib.auth',  
-      'django.contrib.contenttypes',  
-      'django.contrib.sessions',  
-      'django.contrib.messages',  
-      'django.contrib.staticfiles',  
-      'blog',  
-      'crispy_forms',  
-      'crispy_bootstrap5',
-      'blango_auth',  
-      'rest_framework',
-      'rest_framework.authtoken',
-      'drf_yasg',
-      'django_filters',
-  ]
-
-
-  MIDDLEWARE = [
-      'django.middleware.security.SecurityMiddleware',  
-      'django.contrib.sessions.middleware.SessionMiddleware', 
-      'django.middleware.common.CommonMiddleware', 
-      #'django.middleware.csrf.CsrfViewMiddleware',  
-      'django.contrib.auth.middleware.AuthenticationMiddleware', 
-      'django.contrib.messages.middleware.MessageMiddleware',
-
-  ]
-  '''
-  Reminder: MIDDLEWARE COMMENT OUT these changes only apply to working with Django on Codio. Do
-  not make these changes to a project you plan on making available on the
-  internet.
-  '''
-
-  ROOT_URLCONF = 'blango.urls'
-
-
-  TEMPLATES = [
-      {
-          'BACKEND':  'django.template.backends.django.DjangoTemplates', 
-          "DIRS": [BASE_DIR / "templates"],  
-          'APP_DIRS': True,  
-          'OPTIONS': { 
-              'context_processors': [  
-                  'django.template.context_processors.debug', 
-                  'django.template.context_processors.request',
-                  'django.contrib.auth.context_processors.auth',  
-                  'django.contrib.messages.context_processors.messages', 
-                  ],  
-              }, 
-                  
-      },  
-  ]
-
-
-  WSGI_APPLICATION = 'blango.wsgi.application'  
-
-
-  # Database  
-  # https://docs.djangoproject.com/en/3.2/ref/settings/#databases  
-
-  DATABASES = { 
-      'default': {  
-          'ENGINE': 'django.db.backends.sqlite3', 
-          'NAME': BASE_DIR / 'db.sqlite3',  
-          }
-      }
-
-
-  # Password validation  
-  # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators  
-
-  AUTH_PASSWORD_VALIDATORS = [
-      {
-          'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-      },
-      {
-          'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-      },
-      {
-          'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-      },
-      {
-          'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-      },
-  ]
-
-
-  # Internationalization  
-  # https://docs.djangoproject.com/en/3.2/topics/i18n/  
-
-  LANGUAGE_CODE = 'en-us'  
-  TIME_ZONE = ("UTC")  
-  USE_I18N = True  
-  USE_L10N = True  
-  USE_TZ = True  
-
-  # Static files (CSS, JavaScript, Images)  
-  # https://docs.djangoproject.com/en/3.2/howto/static-files/  
-
-  STATIC_URL = '/static/'  
-
-  # Default primary key field type  
-  # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field  
-
-
-  DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  
-  CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"  
-  CRISPY_TEMPLATE_PACK = "bootstrap5" 
-
-  REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-      "rest_framework.authentication.BasicAuthentication",
-      "rest_framework.authentication.SessionAuthentication",
-      "rest_framework.authentication.TokenAuthentication",
-    ],
-
-    "DEFAULT_PERMISSION_CLASSES": [
-      "rest_framework.permissions.IsAuthenticatedOrReadOnly"
-    ],
-
-    #throttling
-    "DEFAULT_THROTTLE_CLASSES": [
-      "blog.api.throttling.AnonSustainedThrottle",
-      "blog.api.throttling.AnonBurstThrottle",
-      "blog.api.throttling.UserSustainedThrottle",
-      "blog.api.throttling.UserBurstThrottle",
-    ],
-    "DEFAULT_THROTTLE_RATES": {
-      "anon_sustained": "500/day",
-      "anon_burst": "10/minute",
-      "user_sustained": "5000/day",
-      "user_burst": "100/minute",
-    },
-
-#pagination
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 100,
-
-
-#extending with django filters for rest rest_framework
-    "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend",
-        "rest_framework.filters.OrderingFilter",
-    ],
-  
-  }
+    AUTH_USER_MODEL = "blango_auth.User"
 
 
 
-SWAGGER_SETTINGS = {
-    "SECURITY_DEFINITIONS": {
-        "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
-        "Basic": {"type": "basic"},
+    #X_FRAME_OPTIONS = 'ALLOW-FROM ' +  os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'  
+    CSRF_COOKIE_SAMESITE = None  
+    #CSRF_TRUSTED_ORIGINS = [os.environ.get('CODIO_HOSTNAME') +  '-8000.codio.io']  
+    CSRF_COOKIE_SECURE = True  
+    SESSION_COOKIE_SECURE = True  
+    CSRF_COOKIE_SAMESITE = 'None'  
+    SESSION_COOKIE_SAMESITE = 'None'  
+
+
+    # Application definition  
+    INSTALLED_APPS = [ 
+        'django.contrib.admin',  
+        'django.contrib.auth',  
+        'django.contrib.contenttypes',  
+        'django.contrib.sessions',  
+        'django.contrib.messages',  
+        'django.contrib.staticfiles',  
+        'blog',  
+        'crispy_forms',  
+        'crispy_bootstrap5',
+        'blango_auth',  
+        'rest_framework',
+        'rest_framework.authtoken',
+        'drf_yasg',
+        'django_filters',
+    ]
+
+
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',  
+        'django.contrib.sessions.middleware.SessionMiddleware', 
+        'django.middleware.common.CommonMiddleware', 
+        #'django.middleware.csrf.CsrfViewMiddleware',  
+        'django.contrib.auth.middleware.AuthenticationMiddleware', 
+        'django.contrib.messages.middleware.MessageMiddleware',
+
+    ]
+
+    '''
+    Reminder: MIDDLEWARE COMMENT OUT these changes only apply to working with Django on Codio. Do
+    not make these changes to a project you plan on making available on the
+    internet.
+    '''
+
+    ROOT_URLCONF = 'blango.urls'
+
+
+    TEMPLATES = [
+        {
+            'BACKEND':  'django.template.backends.django.DjangoTemplates', 
+            "DIRS": [BASE_DIR / "templates"],  
+            'APP_DIRS': True,  
+            'OPTIONS': { 
+                'context_processors': [  
+                    'django.template.context_processors.debug', 
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',  
+                    'django.contrib.messages.context_processors.messages', 
+                    ],  
+                }, 
+                    
+        },  
+    ]
+
+
+    WSGI_APPLICATION = 'blango.wsgi.application'  
+
+
+    # Database  
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#databases  
+
+    DATABASES = { 
+        'default': {  
+            'ENGINE': 'django.db.backends.sqlite3', 
+            'NAME': BASE_DIR / 'db.sqlite3',  
+            }
+        }
+
+
+    # Password validation  
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators  
+
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
+
+
+    # Internationalization  
+    # https://docs.djangoproject.com/en/3.2/topics/i18n/  
+
+    LANGUAGE_CODE = 'en-us'  
+    TIME_ZONE = ("UTC")  
+    USE_I18N = True  
+    USE_L10N = True  
+    USE_TZ = True  
+
+    # Static files (CSS, JavaScript, Images)  
+    # https://docs.djangoproject.com/en/3.2/howto/static-files/  
+
+    STATIC_URL = '/static/'  
+
+    # Default primary key field type  
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field  
+
+
+    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  
+    CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"  
+    CRISPY_TEMPLATE_PACK = "bootstrap5" 
+
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        ],
+
+        "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+        ],
+
+        #throttling
+        "DEFAULT_THROTTLE_CLASSES": [
+        "blog.api.throttling.AnonSustainedThrottle",
+        "blog.api.throttling.AnonBurstThrottle",
+        "blog.api.throttling.UserSustainedThrottle",
+        "blog.api.throttling.UserBurstThrottle",
+        ],
+        "DEFAULT_THROTTLE_RATES": {
+        "anon_sustained": "500/day",
+        "anon_burst": "10/minute",
+        "user_sustained": "5000/day",
+        "user_burst": "100/minute",
+        },
+
+    #pagination
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 100,
+
+
+    #extending with django filters for rest rest_framework
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+            "rest_framework.filters.OrderingFilter",
+        ],
+
+    #extending with JWT Json web tokens
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication"
+        ],
+
+
     }
-}
+
+
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
+    }
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -8,6 +8,11 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 
+#adding JWT imports
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+
 
 #from blog.api.views import PostList, PostDetail, UserDetail, TagViewSet
 from blog.api.views import UserDetail, TagViewSet, PostViewSet
@@ -41,6 +46,10 @@ schema_view = get_schema_view(
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
+#adding JWT token urls
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
+
     path("", include(router.urls)),
     ##filtering to be added after router.urls
     path("posts/by-time/<str:period_name>/", PostViewSet.as_view({"get": "list"}), name="posts-by-time",),
