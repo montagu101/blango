bbef1eac68733c076b179585c8949df93cafea2b
distance <from_a_distance@hispeed.ch>
2024-05-26 17:54:41 +0200

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -41,7 +41,7 @@ setTimeout(() => {
 
 console.log('After setTimeout')
 
-*/
+
 
 for(let i = 0; i < 10; i += 1) {
   console.log('for loop i: ' + i)
@@ -70,3 +70,57 @@ const doubled = numbers.map(value => value * 2)
 console.log('Here are the doubled numbers')
 
 console.log(doubled)
+
+*/
+
+class Greeter {
+  constructor (name) {
+    this.name = name
+  }
+
+  getGreeting () {
+    if (this.name === undefined) {
+      return 'Hello, no name'
+    }
+
+    return 'Hello, ' + this.name
+  }
+
+  showGreeting (greetingMessage) {
+    console.log(greetingMessage)
+  }
+
+  greet () {
+    this.showGreeting(this.getGreeting())
+  }
+}
+
+const g = new Greeter('Patchy')  // Put your name here if you like
+g.greet()
+
+
+
+class DelayedGreeter extends Greeter {
+  delay = 2000
+
+  constructor (name, delay) {
+    super(name)
+    if (delay !== undefined) {
+      this.delay = delay
+    }
+  }
+
+  greet () {
+    setTimeout(
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )
+  }
+}
+
+const dg2 = new DelayedGreeter('Patso 2 Seconds')
+dg2.greet()
+
+const dg1 = new DelayedGreeter('Patchu 1 Second', 1000)
+dg1.greet()
